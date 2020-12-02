4e61227daa7ab058e0278464ea36c154445302a6
Guilherme Mulato <guilherme_mulato@hotmail.com>
2020-12-02 11:02:44 -0300

    Realizando buscas na aplicação

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import api from "./api";
+
 class App {
   //construtor
   constructor() {
@@ -7,6 +9,9 @@ class App {
     //form
     this.formulario = document.querySelector("form");
 
+    //lista
+    this.lista = document.querySelector(".list-group");
+
     //metodo para registrar eventos no form
     this.registrarEventos();
   }
@@ -15,20 +20,84 @@ class App {
     this.formulario.onsubmit = (evento) => this.adicionarRepositorio(evento);
   }
 
-  adicionarRepositorio(evento) {
+  async adicionarRepositorio(evento) {
     // Evita que o formulario recarregue a pagina
     evento.preventDefault();
 
+    //recuperrar valor do input
+    let input = document.querySelector("input[id=repositorio]").value;
+
+    //Se o input vier vazio sai da app
+    if (input.length === 0) {
+      return; //return sempre sai da funcao
+    }
+
+    let response = await api.get(`/repos/${input}`);
+    // console.log(response);
+
+    let {
+      name,
+      description,
+      html_url,
+      owner: { avatar_url },
+    } = response.data;
+
     //Adicona o repositorio na lista
     this.repositorios.push({
-      nome: "Guilherme",
-      descricao: "Developer",
-      avatar_url: "https://avatars0.githubusercontent.com/u/8083459?v=4",
-      link: "https://github.com/ryanoasis/nerd-fonts",
+      nome: name,
+      descricao: description,
+      avatar_url,
+      link: html_url,
     });
 
     // Renderizar a tela
-    console.log(this.repositorios);
+    this.renderizarTela();
+  }
+
+  renderizarTela() {
+    //limpar lista
+    this.lista.innerHTML = "";
+
+    //percorrer lista de repositorios e criar elementos
+    this.repositorios.forEach((repositorio) => {
+      //li
+      let li = document.createElement("li");
+      li.setAttribute("class", "list-group-item list-group-item-action");
+
+      //img
+      let img = document.createElement("img");
+      img.setAttribute("src", repositorio.avatar_url);
+      li.appendChild(img);
+
+      //strong
+      let strong = document.createElement("strong");
+      let txtNome = document.createTextNode(repositorio.nome);
+      strong.appendChild(txtNome);
+      li.appendChild(strong);
+
+      //p
+      let p = document.createElement("p");
+      let txtDescricao = document.createTextNode(repositorio.descricao);
+      p.appendChild(txtDescricao);
+      li.appendChild(p);
+
+      //a
+      let a = document.createElement("a");
+      a.setAttribute("target", "_blank");
+      a.setAttribute("href", repositorio.link);
+      let txtA = document.createTextNode("Acessar");
+      a.appendChild(txtA);
+      li.appendChild(a);
+
+      //adciona tudo na lista
+      this.lista.appendChild(li);
+
+      //limpar conteudo input
+      this.formulario.querySelector("input[id=repositorio]").value = "";
+
+      //adiciona o focus no input
+      this.formulario.querySelector("input[id=repositorio]").focus();
+    });
   }
 }
 
